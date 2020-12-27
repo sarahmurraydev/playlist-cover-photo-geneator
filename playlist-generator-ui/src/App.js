d812741fb4cfd7114c52793ed7aa36807056543e
Sarah Murray <sarahmurraydev@gmail.com>
2020-12-27 18:40:15 -0500

    move api back to main directory for now to get flask working

--- playlist-generator-ui/src/App.js
+++ playlist-generator-ui/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import camera from './camera.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={camera} className="App-logo" alt="camera-icon" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Create new mosiac cover photos for your spotify playlists
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:5000/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Authorize Your Spotify to Get Started
         </a>
       </header>
+      <footer>
+        <div>Icons made by 
+          <a href="https://www.flaticon.com/free-icon/camera_3617267?related_item_id=3617277&term=camera" 
+          title="DinosoftLabs">
+            DinosoftLabs
+          </a> 
+          from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+          </div>
+      </footer>
     </div>
   );
 }
