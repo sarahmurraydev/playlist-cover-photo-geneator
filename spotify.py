d649e9185c2eb856e634cb8a3f389f574881ad6f
Sarah Murray <sarahmurraydev@gmail.com>
2020-12-29 23:11:44 -0500

    add well defined response for PUT profile image and error handling TODOs

--- spotify.py
+++ spotify.py
@@ -14,12 +14,14 @@ def make_auth_header(string):
 def get_me(auth):
     auth_header = make_auth_header(auth)
     user_data = requests.get(GET_ME, headers=auth_header)
+    # TODO: handle error
     return json.loads(user_data.text)
 
 def get_public_playlists(limit, offset, auth): 
     auth_header = make_auth_header(auth)
     uri = GET_PUBLIC_PLAYLISTS.format(limit, offset)
     playlists = requests.get(uri, headers=auth_header)
+    # TODO: handle error
     return json.loads(playlists.text)
 
 # get a playlist items from playlist ID 
@@ -27,15 +29,18 @@ def get_playlist_items(playlist_id, auth):
     auth_header = make_auth_header(auth)
     uri = GET_PLAYLIST_ITEMS.format(playlist_id)
     playlist_items = requests.get(uri, headers=auth_header)
+    # TODO: handle error
     return json.loads(playlist_items.text)
 
 # get album covers for the items from the playlist
 def get_album_cover_photos(album_id, auth):
     image_urls = []
     data = get_playlist_items(album_id, auth=auth)
+    # TODO: handle if there are no items
     tracks = data["items"]
     for i in range(len(tracks)):
         # get the 300 x 300 image url
+        # TODO: handle if there is no url (which there aren't for local files)
         image_url = tracks[i]["track"]["album"]["images"][2]["url"]
         # check if this image is in our array already 
         if image_url in image_urls: 
@@ -57,4 +62,14 @@ def put_playlist_photo(playlist_id, auth, image_as_bytes):
     headers['Content-Type'] = 'image/jpeg'
     image_b64 = base64.b64encode(image_as_bytes)
     response = requests.put(uri, data = image_b64, headers=headers)
-    return response.text
+    response_dict = { 'response_code': 202 }
+    if response.status_code == 202:
+        message = "Successfully posted a photo to ".format(playlist_id)
+        response_dict['message'] = message
+        response_dict['status'] = "success"
+    else: 
+        message = "Error posting a photo to ".format(playlist_id)
+        response_dict['message'] = message
+        response_dict['status'] = "error"
+        
+    return response_dict
