0e58fcbe12d3e7b3fd7d05cbf105b33352a965be
Sarah Murray <sarahmurraydev@gmail.com>
2020-12-30 19:30:22 -0500

    remove tooltip

--- playlist-generator-ui/src/SpotifyDataComponents/MorePlaylistsButton.js
+++ playlist-generator-ui/src/SpotifyDataComponents/MorePlaylistsButton.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import InfoIcon from '@material-ui/icons/Info';
 import { getUserPlaylists } from '../actions/actionCreators';
 
 class MorePlaylistsButton extends React.Component {
@@ -12,13 +11,6 @@ class MorePlaylistsButton extends React.Component {
 
         return <div className="playlist-grid-footer-buttons">
             <button onClick={() => getNextPlaylists(offset)}>Get More of My Playlists</button>
-            <div className="tooltip">
-                <InfoIcon/>
-                <span className="tooltiptext">
-                    The Spotify API only returns 20 of your public playlists at a time. 
-                    To see more of your playlists click the button to the left.
-                </span>
-            </div>
         </div>
     }
 }
