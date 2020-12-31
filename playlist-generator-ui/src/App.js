846a5ea4b10121fa410a4bc65bda1ee08c3d30de
Sarah Murray <sarahmurraydev@gmail.com>
2020-12-30 20:50:06 -0500

    Add header

--- playlist-generator-ui/src/App.js
+++ playlist-generator-ui/src/App.js
@@ -9,29 +9,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Auth from './LandingScreen/Auth'
 import Main from './LandingScreen/Main'
+import Header from './LandingScreen/Header'
 
 function App() {
   return (
-    <Router>
-     <Switch>
-       <Route path={`/authorized/:token`} component={Main}/>
-       <Route path="/" component={Home}/>
-      </Switch>
-    </Router>
+    <div className="App">
+      <Header/>
+      <Router>
+        <Switch>
+          <Route path={`/authorized/:token`} component={Main}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
 function Home(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={camera} className="App-logo" alt="camera-icon" />
+    <div className="home">
+      <img src={camera} className="App-logo" alt="camera-icon" />
         <p>
           Create new mosiac cover photos for your spotify playlists
         </p>
         <Auth/>
-      </header>
-      <footer>
+      {/* <footer>
         <div>Icons made by 
           <a href="https://www.flaticon.com/free-icon/camera_3617267?related_item_id=3617277&term=camera" 
           title="DinosoftLabs">
@@ -39,7 +41,7 @@ function Home(){
           </a> 
           from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
           </div>
-      </footer>
+      </footer> */}
     </div>
   );
 }
---
+++ playlist-generator-ui/src/LandingScreen/Header.js
@@ -0,0 +1,10 @@
+import AppsIcon from '@material-ui/icons/Apps';
+
+const Header = props => (
+    <div className="header">
+        <AppsIcon className="mosaic-icon"/>
+        <h4>Mosaic Playlist Cover Photo Maker</h4>
+    </div>
+)
+
+export default Header
